0562bda39d153e24f8d8c9b771c9a0df489f5131
Jovanna <jovannab147@gmail.com>
2023-04-01 01:58:58 -0400

    commiting

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
+from .models import CarMake, CarModel
 # from .models import related models
 
 
 # Register your models here.
+admin.site.register(CarMake)
+admin.site.register(CarModel)
 
 # CarModelInline class
+class CarModelInline(admin.StackedInline):
+    model = CarModel
+    extra = 1
 
 # CarModelAdmin class
 
